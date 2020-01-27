0786eb657f0c70f8f3a41ad9aa2da199b19c292a
emersomcarvalho <show.021@hotmail.com>
2020-01-27 00:25:17 -0200

    Ajuste local

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ mongoose.connect('mongodb+srv://projecttela:123qwe.@projecttela-jjxo5.mongodb.ne
 useNewUrlParser: true, useUnifiedTopology: true,})
 
 
-app.use(cors())
 app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
