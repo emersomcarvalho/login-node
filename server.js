d42d10957d6168e8f246495da2d8a43789f4c1fa
emersomcarvalho <show.021@hotmail.com>
2020-02-02 15:19:38 -0200

    Ajuste Local

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors());
+app.use(cors({origin: 'https://teladelogin.herokuapp.com/' }));
 app.use(express.json());
 
 require('./src/controllers/authController')(app);
